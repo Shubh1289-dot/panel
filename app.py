9fb55a7f10af18a369aca3387a14e826e29a5c65
Shubh1289-dot <shubhshankar0512@gmail.com>
2026-03-07 11:07:25 +0530

    Update app.py

--- app.py
+++ app.py
@@ -9,8 +9,8 @@ BLOCKED_IPS = [
     "49.37.65.4"
 ]
 LICENSE_KEYS = {
-    "SHUBH": {"hwid": ""}
-    
+    "SHUBH": {"hwid": ""},
+    "HARSH": {"hwid": ""}
 }
 DISCORD_WEBHOOK = "https://discord.com/api/webhooks/1474997412786471054/5jkd8YM6kDPA_xA-u26EFAz0kld1AMClwDn3d4fCg9SztHYFUOqS_8OiDdQbs3jwE1xo"
 
