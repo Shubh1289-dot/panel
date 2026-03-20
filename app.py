119ec972ae27c6358a367a5a2713b5fb258c5021
Shubh1289-dot <shubhshankar0512@gmail.com>
2026-03-20 18:36:46 +0530

    Update app.py

--- app.py
+++ app.py
@@ -676,5 +676,5 @@ def get_messages():
 
 # -------------------- RUN --------------------
 
-if _name_ == "_main_":
-    app.run(debug=True)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=True)
